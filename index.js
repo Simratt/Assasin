f192237173d841a84e2e3472cdef39afc0b1a1f0
Simratt <47543274+Simratt@users.noreply.github.com>
2020-01-19 04:18:18 -0800

    Server is up, need to configure backend

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+//serverstuff
+console.log('server is starting');
+
+var express = require('express');
+var app = express();
+var server = app.listen(3000, listening);
+function listening(){
+    console.log("listening...");
+}
+
+app.use(express.static('data'));
+
+//DiscordBot
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'NjY4MzE0MjQ5ODE4NDA2OTIz.XiP_7g.LqRlq_Z9omtoApZ89cIAHVRTGP8';
+const token = 'NjY4MzE0MjQ5ODE4NDA2OTIz.XiRCJQ.apYqFgm7mAFmwrSOT5rsg2oPhYQ';
 const prefix = '~';
 
 bot.on('ready', ()=>{
@@ -11,9 +24,16 @@ bot.on('message', message=>{
     let args = message.content.substring(prefix.length).split(" ");
     switch(args[0]){
         case "start":
+            var players_list = [];
             try {
-                message.channel.members.forEach(member => {
-                    member.send("Welcome to the game")
+                message.guild.members.forEach(member => {
+                    var player = {
+                        name: member.name,
+                        id: generateId(),
+                        statue: true
+                    }
+                    member.send("Welcome to the game, this is your ID: ".concat(player.id))
+                    players_list.push(player)
                 })
             } catch (e) {
                 console.log(e);
@@ -22,6 +42,21 @@ bot.on('message', message=>{
             console.log("done");
         }
     }
+    app.get('/start', game_start);
+    function game_start(request, response){
+        response.send(players_list);
+    }
     
 })
+
+function generateId() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
 bot.login(token);
