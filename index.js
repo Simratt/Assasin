d2f9c232acf6bce1c844c10ff129f42fcd672a36
Simratt <47543274+Simratt@users.noreply.github.com>
2020-01-18 21:42:23 -0800

    First command operational

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'NjY4MzE0MjQ5ODE4NDA2OTIz.XiPjzA.eL7KFJJGyKcVbxnwrjLL7_LeihA';
+const token = 'NjY4MzE0MjQ5ODE4NDA2OTIz.XiPnnA.u0pOTpho-12gUknIqlf5f4VtIHs';
+const prefix = '~';
 
 bot.on('ready', ()=>{
     console.log('Ready for deployment!');
 })
 
+bot.on('message', message=>{
+    let args = message.content.substring(prefix.length).split(" ");
+    switch(args[0]){
+        case "cash?":
+            setTimeout(function msg(){
+                message.channel.send('GET YOUR OWN CASH ಠ_ಠ')
+            }, 1000);
+        break;
+    }
+})
 bot.login(token);
