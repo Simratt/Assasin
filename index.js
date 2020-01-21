31cce3fbb484dcbf44864a8448c5ffd1eea3d334
Simratt <47543274+Simratt@users.noreply.github.com>
2020-01-20 20:37:38 -0500

    Changed the prefix to "." for easier use

--- index.js
+++ index.js
@@ -13,31 +13,29 @@ app.use(express.static('data'));
 //DiscordBot
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'NjY4MzE0MjQ5ODE4NDA2OTIz.XiRRjA.arLeHVLb1nilwSf72cwlHKik2bc';
-const prefix = '~';
+const token = 'NjY4MzE0MjQ5ODE4NDA2OTIz.XiYJVQ._Il-j1lxdvrLjTrOSoKmys6gxkM';
+const prefix = '.';
 
 bot.on('ready', ()=>{
     console.log('Ready for deployment!');
 })
 
-bot.on('message', message=>{
+bot.on('message', async message=>{
     let args = message.content.substring(prefix.length).split(" ");
+    //members = message.channel.members.values()
+    players = []
     switch(args[0]){
-        case "start":
-            var players_list = [];
-            try {
-                message.guild.members.forEach(member => {
-                    var player = {
-                        name: member.name,
-                        id: generateId(),
-                        statue: true
+        case "send":
+            try{
+                message.channel.members.forEach(member => {
+                    if(member['user']['username'] != 'BAWSE'){
+                        member.send('Welcome ' + member['user']['username'] + ' ! The game will begin shortly')
                     }
-                    member.send("Welcome to the game, this is your ID: ".concat(player.id))
-                    players_list.push(player)
                 })
-            } catch (e) {
-                console.log(e);
-            }
+
+            }catch (e){
+                {}
+            } 
         break;
         case "help":
             message.channel.send("Help is on the way")
@@ -46,13 +44,16 @@ bot.on('message', message=>{
             message.channel.send("The Rules of Assassin are simple \n -You will recieve a target tha you must eliminate \n - You are only allowd to take out your own target, killing anyone else will result in you getting eliminated\n- Once you have achieved your objective, you will be rewarded, and you will recieve a new target \n- Last assassin remaining will win the game")
         break;
         case 'admin_quit':
-                try {
-                    message.guild.members.forEach(member => {
-                        member.send("Game Over, Thanks for playing")
-                    })
-                } catch (e) {
-                    console.log(e);
-                }
+            try{
+                message.channel.members.forEach(member => {
+                    if(member['user']['username'] != 'BAWSE'){
+                        member.send('Game Over, thanks for playing!')
+                    }
+                })
+
+            }catch (e){
+                {}
+            } 
     
         console.log('game over');
     }
@@ -61,6 +62,42 @@ bot.on('message', message=>{
         response.send(players_list);
     }
     
+})
+
+bot.on('message', async message =>{
+    let args = message.content.substring(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+    if(command === ".kick") {
+        // This command must be limited to mods and admins. In this example we just hardcode the role names.
+        // Please read on Array.some() to understand this bit: 
+        // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+        if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+          return message.reply("Sorry, you don't have permissions to use this!");
+        
+        // Let's first check if we have a member and if we can kick them!
+        // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+        // We can also support getting the member by ID, which would be args[0]
+        let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if(!member){
+          return message.reply("Please mention a valid member of this server");
+        }
+        
+        if(!member.kickable) {
+          return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+        }
+        
+        // slice(1) removes the first part, which here should be the user mention or ID
+        // join(' ') takes all the various parts to make it a single string.
+        let reason = args.slice(1).join(' ');
+        if(!reason){
+            reason = "No reason provided";
+        }
+        // Now, time for a swift kick in the nuts!
+        member.kick(reason)
+          .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+        message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+    
+      }
 })
 
 function generateId() {
